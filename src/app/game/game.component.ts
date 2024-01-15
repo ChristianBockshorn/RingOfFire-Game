0d9a9f51ba1b37f50632fc10734db9649eba8e30
Christian Bockshorn <bockshorn@gmx.net>
2024-01-15 18:48:50 +0100

    edit stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from './../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -10,6 +11,17 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
   pickCardAnimation = false;
+  game: Game;
+
+  constructor() {
+    this.game = new Game();
+    console.log(this.game)
+  }
+
+
+  
+
+ 
 
   takeCard() {
     this.pickCardAnimation = true;
